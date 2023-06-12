3f2d1d0abd907ac2b895f9b8db7d81645c234872
paulg44 <paulgarton1984@hotmail.com>
2023-06-12 16:08:10 +0100

    make logo center, fix first test

--- finalproject/src/pages/Homepage/Homepage.test.js
+++ finalproject/src/pages/Homepage/Homepage.test.js
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
 
 import Homepage from "./Hompage.js";
+import { MemoryRouter } from "react-router-dom";
 
 test("renders homepage logo", () => {
   // Renders Page
-  render(<Homepage />);
+  render(
+    <MemoryRouter>
+      <Homepage />
+    </MemoryRouter>
+  );
   //   Check if Logo renders
   const groupBtn = screen.getByAltText("logo");
   //   Test
--- finalproject/src/pages/Homepage/Hompage.js
+++ finalproject/src/pages/Homepage/Hompage.js
@@ -3,13 +3,11 @@
 import "./Hompage.css";
 import { Link } from "react-router-dom";
 import Button from "../../Components/Button/Button";
-import logoImage from "../../Assets/blue.PNG";
+import logoImage from "../../Assets/logo_test_2.jpg";
 
 function Homepage() {
   return (
     <div className="homepage">
-      <h1>Hello</h1>
-    
       <img className="logo" src={logoImage} alt="logo" />
       <Button btnText="Go Solo" />
       <Link to="/create-join">
