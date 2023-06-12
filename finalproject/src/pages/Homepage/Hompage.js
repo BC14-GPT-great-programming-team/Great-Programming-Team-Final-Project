2a49e362d170988999194b78bc4223389b2e497d
Tahmina Begum <tahminabee@hotmail.co.uk>
2023-06-12 15:59:00 +0100

    logo image

--- finalproject/src/pages/Homepage/Hompage.js
+++ finalproject/src/pages/Homepage/Hompage.js
@@ -3,12 +3,14 @@
 import "./Hompage.css";
 import { Link } from "react-router-dom";
 import Button from "../../Components/Button/Button";
+import logoImage from "../../Assets/blue.PNG";
 
 function Homepage() {
   return (
     <div className="homepage">
       <h1>Hello</h1>
-      <img className="logo" alt="logo" />
+    
+      <img className="logo" src={logoImage} alt="logo" />
       <Button btnText="Go Solo" />
       <Link to="/create-join">
         <Button btnText="Group Mode" />
