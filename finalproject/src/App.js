460d0c15aa98cba8f647c6770582cf92665d42c1
Automoroboto <roberto@studiodespirito.co.uk>
2023-06-12 12:29:05 +0100

    create basic click handlers on buttons

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -1,29 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import VoteScreen from "./pages//VoteScreen/VoteScreen";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Test Header</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VoteScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- finalproject/src/pages/VoteScreen/VoteScreen.js
+++ finalproject/src/pages/VoteScreen/VoteScreen.js
@@ -1,19 +1,40 @@
+
+
+
 // george and robertos work
 //PLAN:
-//import react and react router
-//create function that returns JSX
+//import react and react router DONE!!
+//create function that returns JSX DONE!!
 //   return:
 //        selection choices component
-//logic to update score variable when button is selected?
+
+//logic to update score variable when button is selected?DONE!!
 // pass selection choices props down
+//
 
-export default function voteScreen(){
-    //logic?
-    return (
-        <div>
-            <button className="option">Resturant</button>
-        </div>
-    )
+import React, { useState } from 'react';
+
+export default function VoteScreen() {
+  const [score, setScore] = useState(0);
+
+  function handleClick() {
+    setScore(score + 1);
+    console.log(score);
+  }
+
+  function handleNext() {
+    setScore(0);
+    console.log(score);
+  }
+
+  return (
+    <div>
+      <button className="option" onClick={handleClick}>
+        Restaurant
+      </button>
+      <button className="next_button" onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
 }
-
-
