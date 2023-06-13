31b4dbfbb3a25a1848d83ba93e4ba07a3cb0ddff
mia downe <miadowne@yahoo.com>
2023-06-13 12:15:42 +0100

    futher testing on homepage

--- finalproject/src/pages/Homepage/Homepage.test.js
+++ finalproject/src/pages/Homepage/Homepage.test.js
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Homepage from "./Hompage.js";
 import { MemoryRouter } from "react-router-dom";
 
+
 test("renders homepage logo", () => {
   // Renders Page
   render(
@@ -15,3 +16,19 @@ test("renders homepage logo", () => {
   //   Test
   expect(groupBtn).toBeInTheDocument();
 });
+
+// jest.mock('../../Components/Button/Button.js', () => () => (<button>click</button>));
+//the mock covers over the existing functionality and test will test for mock instead of the existing function
+
+test( "renders buttons", () => {
+ const {container} = 
+ render(
+    <MemoryRouter>
+      <Homepage />
+    </MemoryRouter>
+  );
+
+expect( container.textContent).toMatch("Go SoloGroup ModeSpin The Wheel")
+  }
+
+)
