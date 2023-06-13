d3d46e4ea3d3d8b16e690c4c0eba10e42fe301f6
paulg44 <paulgarton1984@hotmail.com>
2023-06-13 16:39:03 +0100

    fix results

--- finalproject/src/pages/Results/Results.js
+++ finalproject/src/pages/Results/Results.js
@@ -1,39 +1,36 @@
+import { React } from "react";
+import { Link } from "react-router-dom";
 
-import {React} from 'react';
-import {Link} from 'react-router-dom';
-
-export default function Results({handleNextRound, rounds, roundCount, venueData}){
-//const [roundResult, setRoundResult] = useState("");
-
+export default function Results({
+  handleNextRound,
+  rounds,
+  roundCount,
+  venueData,
+}) {
+  //const [roundResult, setRoundResult] = useState("");
 
   // for (let i = 0; i < rounds.length; i++) {
   //   for (let j = 0; i < rounds[j].length; j++){
   //   if (rounds[i][j].score === 1) {
 
-
-
-
-const voteResults = rounds.map((round) => {
-  return round.map((option) => {
-   if (option.score >= 1) {
-    return option.name;
-  } else {
-    return null;
-  }
-
+  const voteResults = rounds.map((round) => {
+    return round.map((option) => {
+      if (option.score >= 1) {
+        return option.name;
+      } else {
+        return null;
+      }
+    });
   });
 
   //console.log(voteResults)
   return (
     <div>
-
-    <div>
-      <h1>Results Page</h1>
-      <h1>{voteResults[roundCount]} is the winner!</h1>
-    </div>
-        <button onClick={handleNextRound}>Next</button>
-    
-
+      <div>
+        <h1>Results Page</h1>
+        <h1>{voteResults[roundCount]} is the winner!</h1>
+      </div>
+      <button onClick={handleNextRound}>Next</button>
     </div>
   );
 }
