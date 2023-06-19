d59ec1168c76c094c5547874665bccaa6eab0142
mia downe <miadowne@yahoo.com>
2023-06-19 15:17:23 +0100

    continuation of vote-handler

--- finalproject/functions/votehandler/votehandler.js
+++ finalproject/functions/votehandler/votehandler.js
@@ -1,13 +1,29 @@
 //this is the vote handler function that will work on the server
 
+const{createClient} = require('@supabase/supabase-js')
+
 exports.handler = async function (event, context) {
     try {
-        const data = {message: "Hello from the serverless function"};
+        const supabase = createClient(process.env.REACT_APP_SUPABASE_URL,process.env.REACT_APP_SUPABASE_KEY)
+        const {data, error}= await supabase.from('users').select('*')
+
+        if (error) {
+            console.error('Supabase error:', error);
+            return {
+              statusCode: 500,
+              body: JSON.stringify({ error: 'Something went wrong with Supabase' }),
+            };
+          }
+
+          const testData = {
+            message: 'Hello from the serverless function',
+            data,
+          };
 
         return {
             statusCode: 200,
             headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(data),
+            body: JSON.stringify(testData),
         };
     } catch (error) {
         console.error(`Error occurred`, error);
