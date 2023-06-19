743575dfba4bf8b754e51583751afc71d596123a
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-19 16:36:14 +0100

    Typo in Netlify functions address

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -18,7 +18,7 @@ import Lobby from "./pages/Lobby/Lobby";
 function App() {
 
   async function fetchVoteHandler() {
-    const response = await fetch(`http://https://consensusgpt.netlify.app/.netlify/functions/votehandler`);
+    const response = await fetch(`https://consensusgpt.netlify.app/.netlify/functions/votehandler`);
     const data = await response.json();
     console.log(data);
   }
