395cf3c67f8e4dcca6433ab56a6ac0cf26a1bded
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-19 15:46:56 +0100

    Several route handlers completed, add user, add group, assign user to group. Tested in Thunderclient

--- finalproject/functions/votehandler/votehandler.js
+++ finalproject/functions/votehandler/votehandler.js
@@ -4,8 +4,21 @@ const{createClient} = require('@supabase/supabase-js')
 
 exports.handler = async function (event, context) {
     try {
-        const supabase = createClient(process.env.REACT_APP_SUPABASE_URL,process.env.REACT_APP_SUPABASE_KEY)
-        const {data, error}= await supabase.from('users').select('*')
+        const supabase = createClient(
+          process.env.REACT_APP_SUPABASE_URL,
+          process.env.REACT_APP_SUPABASE_KEY
+          );
+
+
+        const requestBody = JSON.parse(event.body);
+
+        if (requestBody.type === "createUser")  {
+
+        const {data, error}= await supabase
+        .from('users')
+        .insert({username:requestBody.username, user_email:requestBody.user_email})
+        .single()
+        .select();
 
         if (error) {
             console.error('Supabase error:', error);
@@ -15,17 +28,83 @@ exports.handler = async function (event, context) {
             };
           }
 
-          const testData = {
-            message: 'Hello from the serverless function',
-            data,
+          const responseData = {
+            message: 'New user created',
+            user_id:data.user_id,
           };
 
         return {
             statusCode: 200,
             headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(testData),
+            body: JSON.stringify(responseData),
         };
-    } catch (error) {
+    } else if (requestBody.type === "createGroup") {
+      
+        const {data, error}= await supabase
+        .from('groups')
+        .insert({group_name:requestBody.group_name})
+        .single()
+        .select();
+
+        if (error) {
+            console.error('Supabase error:', error);
+            return {
+              statusCode: 500,
+              body: JSON.stringify({ error: 'Something went wrong with Supabase' }),
+            };
+          }
+
+          const responseData = {
+            message: 'New group created',
+            group_id:data.group_id,
+          };
+
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(responseData),
+        };
+
+    } else if (requestBody.type === "assignUser") {
+      
+      const {data, error} = await supabase
+      .from('users')
+      .update({group_id:requestBody.group_id})
+      .eq('user_id', requestBody.user_id)
+      .single()
+      .select();
+
+      if (error) {
+        console.error('Supabase error:', error);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: 'Something went wrong with Supabase' }),
+        };
+      }
+
+      const responseData = {
+        message: 'User assigned to group',
+        group_id:data.group_id,
+        user_id:data.user_id,
+      };
+
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(responseData),
+    };
+
+
+    } else { 
+      return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({ error: 'Invalid request type' }),
+    };
+    }
+    
+  }
+    catch (error) {
         console.error(`Error occurred`, error);
         return {
             statusCode: 500,
