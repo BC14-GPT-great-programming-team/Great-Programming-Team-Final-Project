44967961355551fdf5d7a979aaead295e8807e7b
Tahmina Begum <tahminabee@hotmail.co.uk>
2023-06-19 14:42:35 +0100

    lobby css

--- finalproject/src/pages/CreateGroup/CreateGroup.js
+++ finalproject/src/pages/CreateGroup/CreateGroup.js
@@ -2,8 +2,13 @@ import "./CreateGroup.css";
 import Button from "../../Components/Button/Button";
 import PreFilterSVG from "../PreFilterPage/PreFilterSVGGreen";
 import { Link } from "react-router-dom";
+import {useState} from "react";
 
 function CreateGroup() {
+
+
+
+
   return (
     <div className="CreateGroup">
       <input placeholder="Enter username"/> 
