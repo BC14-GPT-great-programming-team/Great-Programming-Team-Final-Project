502f1208675e0c68f20d30402e26ef12685f5eff
paulg44 <paulgarton1984@hotmail.com>
2023-06-20 13:54:42 +0100

    filter page input value buttons etc

--- finalproject/src/pages/PreFilterPage/PreFilterPage.test.js
+++ finalproject/src/pages/PreFilterPage/PreFilterPage.test.js
@@ -12,7 +12,6 @@ import PreFilter from "../PreFilterPage/PreFilterPage.js";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 //testing to see if the page can be rendered for the pre filter
-
 test("render of page for the pre filter", () => {
   const { container } = render(
     <MemoryRouter>
@@ -22,22 +21,33 @@ test("render of page for the pre filter", () => {
   expect(container).toBeInTheDocument();
 });
 
-// Test for next button to not be clickable until input has value
+// !!Test for next button to not be clickable until the input has value!!
 test("input value and button state ", () => {
   // Mock input
   const mockedLocation = "London";
 
   render(
     <MemoryRouter>
+      {/* Mock the user inputting London */}
       <PreFilter userInput={{ location: mockedLocation }} />
     </MemoryRouter>
   );
 
+  //   Tests run from top to bottom
+  //   Button Tests
   const nextBtn = screen.getByRole("button", { name: "Next" });
   expect(nextBtn).toBeInTheDocument();
+  expect(nextBtn).toBeDisabled();
 
+  //   Input Tests
   const locationInput = screen.getByRole("textbox");
   expect(locationInput).toBeInTheDocument();
+  expect(locationInput.value).toBe("");
 
-  expect(locationInput).toBe(mockedLocation);
+  //   User Event
+  fireEvent.change(locationInput, { target: { value: mockedLocation } });
+
+  //   Final Test to see if Button is enabled
+  expect(locationInput.value).toBe(mockedLocation);
+  expect(nextBtn).toBeEnabled();
 });
