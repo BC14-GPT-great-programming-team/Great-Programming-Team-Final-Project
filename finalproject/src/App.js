b0912dea7496e3fece43b6485c737a9485abeb89
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-24 23:08:45 +0100

    Vote purging when restarting now working

--- finalproject/src/App.js
+++ finalproject/src/App.js
@@ -22,11 +22,11 @@ function App() {
 
 
 
- // const serverURL = "http://localhost:8888/.netlify/functions/votehandler";
+ const serverURL = "http://localhost:8888/.netlify/functions/votehandler";
 
 //comment out the below line for deployment
  
-const serverURL = "https://consensusgpt.netlify.app/.netlify/functions/votehandler";
+// const serverURL = "https://consensusgpt.netlify.app/.netlify/functions/votehandler";
 
 
 
@@ -129,6 +129,7 @@ const [CurrentGroupResult, setCurrentGroupResult] = useState([]);
     setRounds(initialRounds);
     const userRequestBody = {
       type: "purgeVotes",
+      group_id: groupid,
     };
     fetch(serverURL, {
       method: "POST",
