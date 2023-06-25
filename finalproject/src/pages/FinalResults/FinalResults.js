6877590e4ed948560c3a2d6daf70bd40ad3e9fea
paulg44 <paulgarton1984@hotmail.com>
2023-06-25 16:21:34 +0100

    tired!

--- finalproject/src/pages/FinalResults/FinalResults.js
+++ finalproject/src/pages/FinalResults/FinalResults.js
@@ -2,24 +2,26 @@ import React from "react";
 import "./FinalResults.css";
 import "../../App.css";
 import PreFilterSVG from "../../Components/BackgroundSVG/PreFilterSVGPeach";
-import locationicon from "../../Assets/locationicon.png";
+import { FaLocationArrow } from "react-icons/fa";
 
 export default function FinalResults({ venueData, handleRestart }) {
   return (
     <div className="finalResultsPage">
       <h2 className="enjoy">Enjoy your night out!</h2>
       <div className="resultsCard">
-        <h2 className="resturant">{venueData && venueData[0].venue_name}</h2>
+        <h2 className="restaurantName">
+          {venueData && venueData[0].venue_name}
+        </h2>
 
         <img
           src={venueData && venueData[0].image_url}
           className="restaurant_image"
           alt="restaurant"
         ></img>
-        <img className="location" src={locationicon} alt="locationicon" />
+
         <div className="textMargin">
           <p>{venueData && venueData[0].address}</p>
-
+          <FaLocationArrow className="venueIcon" />
           <p>{venueData && venueData[0].phone_no}</p>
           <p> User rating: {venueData && venueData[0].user_rating}</p>
           <p>{venueData && venueData[0].website_url}</p>
