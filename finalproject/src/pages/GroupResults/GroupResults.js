8680b4f98227fae212c0a66aa0b2bdf1aae7a850
paulg44 <paulgarton1984@hotmail.com>
2023-06-26 12:22:17 +0100

    add CSS to group pages

--- finalproject/src/pages/GroupResults/GroupResults.js
+++ finalproject/src/pages/GroupResults/GroupResults.js
@@ -3,88 +3,100 @@ import { Link } from "react-router-dom";
 import "./GroupResults.css";
 import HomeButton from "../../Components/HomeButton/HomeButton";
 
-export default function Results({ handleNextGroupRound, serverURL, groupid, currentRoundID, setCurrentGroupResult, CurrentGroupResult, setCurrentRoundLabel, setGroupFilter}) {
+export default function Results({
+  handleNextGroupRound,
+  serverURL,
+  groupid,
+  currentRoundID,
+  setCurrentGroupResult,
+  CurrentGroupResult,
+  setCurrentRoundLabel,
+  setGroupFilter,
+}) {
+  // this useEffect is for fetching the votes from the database
+  useEffect(() => {
+    const fetchVotes = () => {
+      const groupUsernamesRequestBody = {
+        type: "getVotes",
+        group_id: groupid,
+        vote_stage: currentRoundID,
+      };
 
+      fetch(serverURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(groupUsernamesRequestBody),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          let roundLabel = data.roundlabel;
+          let resultArray = data.resultArray;
+          let resultArraySorted =
+            resultArray && resultArray.sort((a, b) => b.votes - a.votes);
+          if (roundLabel) {
+            setCurrentRoundLabel(roundLabel);
+          }
+          if (resultArraySorted) {
+            setCurrentGroupResult(resultArraySorted);
+          }
+          if (roundLabel !== undefined && resultArraySorted !== undefined) {
+            setGroupFilter(roundLabel, resultArraySorted[0].choice);
+          }
 
-// this useEffect is for fetching the votes from the database
-    useEffect(() => {
-        const fetchVotes = () => {
-          const groupUsernamesRequestBody = {
-            type: "getVotes", 
-            group_id: groupid,
-            vote_stage: currentRoundID,
-          };
-    
-          fetch(serverURL, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(groupUsernamesRequestBody),
-          })
-          .then((response) => response.json())
-          .then((data) => {
-            let roundLabel = data.roundlabel
-             let resultArray = data.resultArray
-             let resultArraySorted = resultArray && resultArray.sort((a, b) => b.votes - a.votes)
-           if (roundLabel) {setCurrentRoundLabel(roundLabel)}
-           if (resultArraySorted) {setCurrentGroupResult(resultArraySorted)}
-           if (roundLabel !== undefined && resultArraySorted !== undefined) {setGroupFilter(roundLabel, resultArraySorted[0].choice)}
-        
           //  console.log(`this is roundLabel:`)
           // console.log(roundLabel)
           //  console.log(`this is CurrentGroupResult:`)
           // console.log(CurrentGroupResult)
           // console.log(`this is resultArraySorted:`)
           // console.log(resultArraySorted)
-          });
-        };
-        fetchVotes();
-    
-        const interval = setInterval(fetchVotes, 2000);
-    
-        return () => clearInterval(interval);
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [])
+        });
+    };
+    fetchVotes();
 
+    const interval = setInterval(fetchVotes, 2000);
 
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="resultsPage">
       <Link to="/">
         <HomeButton />
       </Link>
-
-      <div className="wavy">
-        <h2>
-        <span style={{"--i": 1 }}>Y</span>
-        <span style={{"--i": 2 }}>o</span>
-        <span style={{"--i": 3 }}>u</span>
-        <br></br>
-        <span style={{"--i": 4 }}>h</span>
-        <span style={{"--i": 5 }}>a</span>
-        <span style={{"--i": 6 }}>v</span>
-        <span style={{"--i": 7} }>e</span>
-        <br></br>
-        <span style={{"--i": 8 }}>c</span>
-        <span style={{"--i": 9 }}>h</span>
-        <span style={{"--i": 10 }}>o</span>
-        <span style={{"--i": 11 }}>s</span>
-        <span style={{"--i": 12 }}>e</span>
-        <span style={{"--i": 13 }}>n</span>
-        <span style={{"--i": 14 }}>:</span>
-        </h2>
-        {CurrentGroupResult && CurrentGroupResult.length > 0 ? (
-          <>
-            <h2>{CurrentGroupResult[0].choice}</h2>
-                {CurrentGroupResult.map((result) => (
-                  <h2>{result.choice}: {result.votes}</h2>
-                  ))}
-                  </>
-        ) : (
-          <h2>Waiting for votes...</h2>
-        )}
-
+      <div className="bubblesContainer">
+        <div className="mainBubble ">
+          <h3 className="topResult">
+            {CurrentGroupResult[0].choice}: {CurrentGroupResult[0].votes}
+          </h3>
+        </div>
+        <div className="secondBubble">
+          {CurrentGroupResult[1].choice}: {CurrentGroupResult[1].votes}
+        </div>
+        <div className="thirdBubble">
+          {CurrentGroupResult[2].choice}: {CurrentGroupResult[2].votes}
+        </div>
+        <div className="bubble one"></div>
+        <div className="bubble two"></div>
+        <div className="bubble three"></div>
+        <div className="bubble four"></div>
+        <div className="bubble five"></div>
+        <div className="bubble six"></div>
+        <div className="bubble seven"></div>
+        <div className="bubble eight"></div>
       </div>
-      
+      {CurrentGroupResult && CurrentGroupResult.length > 0 ? (
+        <>
+          {CurrentGroupResult.map((result) => (
+            <h2>
+              {result.choice}: {result.votes}
+            </h2>
+          ))}
+        </>
+      ) : (
+        <h2>Waiting for votes...</h2>
+      )}
+
       <button className="nextBtn" onClick={handleNextGroupRound}>
         Next
       </button>
