2a2e9e87040bd51024956a3fc204d7f820e75a00
paulg44 <paulgarton1984@hotmail.com>
2023-06-27 14:45:20 +0100

    added aria labels

--- finalproject/src/pages/Homepage/Hompage.js
+++ finalproject/src/pages/Homepage/Hompage.js
@@ -10,59 +10,55 @@ import PreFilterSVG from "../../Components/BackgroundSVG/PreFilterSVGGreen";
 
 function Homepage({ setGroupMode }) {
   return (
-  
     <div className="homepage">
-      <h1 className="h1">concensus, the ultimate decision-maker for indecisive folk</h1>
-      <img className="logo" src={officialLogo} alt="logo" />
+      <h1 className="h1" class="sr-only">
+        concensus, the ultimate decision-maker for indecisive folk
+      </h1>
+      <img
+        className="logo"
+        src={officialLogo}
+        alt="this is the logo for consensUs, a tangle of arms meeting together"
+      />
       {/* <h1>
         Consens<span>U</span>s
       </h1>
       {/* <FaUsers className="icon" /> */}
       {/* <h4>The Ultimate Decision Maker</h4>  */}
 
-      <Link to="/prefilter">
+      <Link to="/prefilter" aria-label="opens pre filter page" role="link">
         <button
           onClick={() => {
             setGroupMode(false);
           }}
           className="topHomeBtn homepageBtn bounceOne"
+          role="button"
         >
           GO SOLO
         </button>
       </Link>
 
-      <Link to="/create-join">
+      <Link to="/create-join" aria-label="opens create join page" role="link">
         <button
           onClick={() => {
             setGroupMode(true);
           }}
           className="homepageBtn bounceTwo"
+          role="button"
         >
           GROUP MODE
         </button>
       </Link>
 
-      {/* <div className="spinBtnContainer">
-        <Button
-          className="homepageBtn bounceThree"
-          btnText="SPIN THE WHEEL   "
-          // icon={<FaDharmachakra />}
-        />
-      </div> */}
-
-
-      <Link to="/needHelp">
-        <Button
-          className="needHelpBtn"
-          btnText="Need help?"
-        />
-        </Link>
-      
+      <Link to="/needHelp" aria-label="opens need help page" role="link">
+        <Button className="needHelpBtn" btnText="Need help?" role="button" />
+      </Link>
 
       <div>
-      <p className="motto">"Saving your friendships one decision at a time!"</p>
-      <PreFilterSVG />
-    </div>
+        <p className="motto" aria-label="tagline">
+          "Saving your friendships one decision at a time!"
+        </p>
+        <PreFilterSVG />
+      </div>
     </div>
   );
 }
