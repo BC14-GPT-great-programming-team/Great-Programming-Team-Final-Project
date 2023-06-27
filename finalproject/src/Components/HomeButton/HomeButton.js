770dab4fde4ff2c1ae49c436b5b1340d3b88e6c4
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-06-27 16:30:00 +0100

    Removed alert from home button

--- finalproject/src/Components/HomeButton/HomeButton.js
+++ finalproject/src/Components/HomeButton/HomeButton.js
@@ -5,15 +5,8 @@ import { FaHome } from "react-icons/fa";
 
 function HomeButton({handleHome}) {
   
-  const handleClick = () => {
-    const confirmed = window.confirm("Votes & username will be reset - are you sure you want to go back? ");
-    if (confirmed) {
-      handleHome();
-    }
-  };
-  
   return (
-    <button className="homeBtn" onClick={handleClick}>
+    <button className="homeBtn" onClick={handleHome}>
       <FaHome />
     </button>
   );
